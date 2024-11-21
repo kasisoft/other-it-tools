8f9da877e93bd8e11671de7701985fcdf8196134
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-11-21 18:30:16 +0100

    ~ added support for the parameter 'mode' in 'defineConfig'

--- vite.config.ts
+++ vite.config.ts
@@ -19,98 +19,100 @@ import { configDefaults } from 'vitest/config';
 const baseUrl = process.env.BASE_URL ?? '/';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    VueI18n({
-      runtimeOnly: true,
-      jitCompilation: true,
-      compositionOnly: true,
-      fullInstall: true,
-      strictMessage: false,
-      include: [
-        resolve(__dirname, 'locales/**'),
-      ],
-    }),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        '@vueuse/core',
-        'vue-i18n',
-        {
-          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
-        },
-      ],
-      vueTemplate: true,
-      eslintrc: {
-        enabled: true,
-      },
-    }),
-    Icons({ compiler: 'vue3' }),
-    vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    vueJsx(),
-    markdown(),
-    svgLoader(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      strategies: 'generateSW',
-      manifest: {
-        name: 'IT Tools',
-        description: 'Aggregated set of useful tools for developers.',
-        display: 'standalone',
-        lang: 'fr-FR',
-        start_url: `${baseUrl}?utm_source=pwa&utm_medium=pwa`,
-        orientation: 'any',
-        theme_color: '#18a058',
-        background_color: '#f1f5f9',
-        icons: [
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      VueI18n({
+        runtimeOnly: true,
+        jitCompilation: true,
+        compositionOnly: true,
+        fullInstall: true,
+        strictMessage: false,
+        include: [
+          resolve(__dirname, 'locales/**'),
+        ],
+      }),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          '@vueuse/core',
+          'vue-i18n',
           {
-            src: '/favicon-16x16.png',
-            type: 'image/png',
-            sizes: '16x16',
-          },
-          {
-            src: '/favicon-32x32.png',
-            type: 'image/png',
-            sizes: '32x32',
-          },
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
+            'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
           },
         ],
+        vueTemplate: true,
+        eslintrc: {
+          enabled: true,
+        },
+      }),
+      Icons({ compiler: 'vue3' }),
+      vue({
+        include: [/\.vue$/, /\.md$/],
+      }),
+      vueJsx(),
+      markdown(),
+      svgLoader(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        strategies: 'generateSW',
+        manifest: {
+          name: 'IT Tools',
+          description: 'Aggregated set of useful tools for developers.',
+          display: 'standalone',
+          lang: 'fr-FR',
+          start_url: `${baseUrl}?utm_source=pwa&utm_medium=pwa`,
+          orientation: 'any',
+          theme_color: '#18a058',
+          background_color: '#f1f5f9',
+          icons: [
+            {
+              src: '/favicon-16x16.png',
+              type: 'image/png',
+              sizes: '16x16',
+            },
+            {
+              src: '/favicon-32x32.png',
+              type: 'image/png',
+              sizes: '32x32',
+            },
+            {
+              src: '/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+      Components({
+        dirs: ['src/'],
+        extensions: ['vue', 'md'],
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        resolvers: [NaiveUiResolver(), IconsResolver({ prefix: 'icon' })],
+      }),
+      Unocss(),
+    ],
+    base: baseUrl,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-    }),
-    Components({
-      dirs: ['src/'],
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [NaiveUiResolver(), IconsResolver({ prefix: 'icon' })],
-    }),
-    Unocss(),
-  ],
-  base: baseUrl,
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  define: {
-    'import.meta.env.PACKAGE_VERSION': JSON.stringify(process.env.npm_package_version),
-  },
-  test: {
-    exclude: [...configDefaults.exclude, '**/*.e2e.spec.ts'],
-  },
-  build: {
-    target: 'esnext',
-  },
+    define: {
+      'import.meta.env.PACKAGE_VERSION': JSON.stringify(process.env.npm_package_version),
+    },
+    test: {
+      exclude: [...configDefaults.exclude, '**/*.e2e.spec.ts'],
+    },
+    build: {
+      target: 'esnext',
+    },
+  };
 });
